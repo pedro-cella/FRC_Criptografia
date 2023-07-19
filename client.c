fa5a5c026222e66b8e29586d8abc6960d06d2df0
Paulo Gonçalves Lima <paulogoncalves436@gmail.com>
2023-07-18 21:58:21 -0300

    Fix: Enviar imagem por TCP, sem cortar a imagem ao meio.
    Co-authored-by: Pedro Cella <49076513+pedro-cella@users.noreply.github.com>

--- client.c
+++ client.c
@@ -1,78 +1,88 @@
-#include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <arpa/inet.h>
 
-#define SIZE 2048
+#define SERVER_IP "127.0.0.1"  // Replace this with the server's IP address
+#define SERVER_PORT 12345      // Replace this with the server's port
 
-void send_file(FILE *fp, int sockfd, int size) {
-  int n;
-  char send_buffer[size];
-  while(!feof(fp)) {
-      fread(send_buffer, 1, sizeof(send_buffer), fp);
-      write(sockfd, send_buffer, sizeof(send_buffer));
-      bzero(send_buffer, sizeof(send_buffer));
-  }
+// Function to read the .bmp file and send it over the TCP connection
+int send_bmp_over_tcp(const char* filename) {
+    FILE* bmp_file = fopen(filename, "rb");
+    if (!bmp_file) {
+        perror("Error opening BMP file");
+        return -1;
+    }
+
+    fseek(bmp_file, 0, SEEK_END);
+    long file_size = ftell(bmp_file);
+    rewind(bmp_file);
+
+    char* buffer = (char*)malloc(file_size);
+    if (!buffer) {
+        perror("Memory allocation error");
+        fclose(bmp_file);
+        return -1;
+    }
+
+    if (fread(buffer, 1, file_size, bmp_file) != file_size) {
+        perror("Error reading BMP file");
+        free(buffer);
+        fclose(bmp_file);
+        return -1;
+    }
+
+    fclose(bmp_file);
+
+    // Create a TCP socket
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        perror("Error creating socket");
+        free(buffer);
+        return -1;
+    }
+
+    // Set up the server address
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(SERVER_PORT);
+    if (inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr) <= 0) {
+        perror("Invalid server IP address");
+        free(buffer);
+        close(sockfd);
+        return -1;
+    }
+
+    // Connect to the server
+    if (connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Connection failed");
+        free(buffer);
+        close(sockfd);
+        return -1;
+    }
+
+    // Send the BMP data
+    if (write(sockfd, buffer, file_size) != file_size) {
+        perror("Error sending BMP data");
+        free(buffer);
+        close(sockfd);
+        return -1;
+    }
+
+    free(buffer);
+    close(sockfd);
+    return 0;
 }
 
 int main(int argc, char *argv[]) {
-  if (argc <= 1) {
-    fprintf(stderr, "ERROR: Coloque o nome do arquivo a ser enviado! \n");
-    return -1;
-  }
+    const char* bmp_filename = argv[1]; // Replace this with the actual .bmp filename
+    if (send_bmp_over_tcp(bmp_filename) == 0) {
+        printf("BMP file sent successfully.\n");
+    } else {
+        printf("Failed to send BMP file.\n");
+    }
 
-  char *ip = "127.0.0.1";
-  int port = 255666;
-  int e;
-
-  int sockfd;
-  struct sockaddr_in server_addr;
-  FILE *fp;
-  char *filename = argv[1];
-
-  sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  if (sockfd < 0) {
-    perror("[-]Error in socket");
-    exit(1);
-  }
-  printf("[+]Server socket created successfully.\n");
-
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_port = port;
-  server_addr.sin_addr.s_addr = inet_addr(ip);
-
-  e = connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
-  if (e == -1) {
-    perror("[-]Error in socket");
-    exit(1);
-  }
-  printf("[+]Connected to Server.\n");
-
-  fp = fopen(filename, "rb");
-  if (fp == NULL) {
-    perror("[-]Error in reading file.");
-    exit(1);
-  }
-  printf("[+]Opened file.\n");
-
-  //Get Picture Size
-  printf("[+]Getting Picture Size\n");
-  int size;
-  fseek(fp, 0, SEEK_END);
-  printf("[+]FSEEK END\n");
-  size = ftell(fp);
-  fseek(fp, 0, SEEK_SET);
-
-  //Send Picture Size
-  printf("[+]Sending Picture Size\n");
-  write(sockfd, &size, sizeof(size));
-
-  send_file(fp, sockfd, size);
-  printf("[+]File data sent successfully.\n");
-
-  printf("[+]Closing the connection.\n");
-  close(sockfd);
-
-  return 0;
+    return 0;
 }
--- server.c
+++ server.c
@@ -1,74 +1,85 @@
-#include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <arpa/inet.h>
 
-#define SIZE 2048
+#define PORT 12345  // Replace this with the port you want the server to listen on
 
-void write_file(int sockfd, int size) {
-  int n;
-  FILE *fp;
-  char *filename = "recived_file.bmp";
+// Function to receive BMP data over the TCP connection and save it to a file
+int receive_bmp_over_tcp() {
+    // Create a TCP socket
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == -1) {
+        perror("Error creating socket");
+        return -1;
+    }
 
-  printf("Reading Picture Byte Array\n");
-  char p_array[size];
-  read(sockfd, p_array, size);
+    // Set up the server address
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
 
-  //Convert it Back into Picture
-  printf("Converting Byte Array to Picture\n");
-  FILE *image;
-  image = fopen("c1.bmp", "w");
-  fwrite(p_array, 1, sizeof(p_array), image);
-  fclose(image);
-  return;
+    // Bind the socket to the specified port
+    if (bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Binding failed");
+        close(server_fd);
+        return -1;
+    }
+
+    // Listen for incoming connections
+    if (listen(server_fd, 1) < 0) {
+        perror("Listening failed");
+        close(server_fd);
+        return -1;
+    }
+
+    // Accept the incoming connection
+    struct sockaddr_in client_addr;
+    socklen_t client_len = sizeof(client_addr);
+    int client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &client_len);
+    if (client_fd < 0) {
+        perror("Acceptance failed");
+        close(server_fd);
+        return -1;
+    }
+
+    // Create a file to save the received BMP data
+    FILE* bmp_file = fopen("received.bmp", "wb");
+    if (!bmp_file) {
+        perror("Error creating file");
+        close(client_fd);
+        close(server_fd);
+        return -1;
+    }
+
+    // Receive the BMP data and save it to the file
+    char buffer[4096];
+    ssize_t bytes_received;
+    while ((bytes_received = read(client_fd, buffer, sizeof(buffer))) > 0) {
+        if (fwrite(buffer, 1, bytes_received, bmp_file) != bytes_received) {
+            perror("Error writing to file");
+            fclose(bmp_file);
+            close(client_fd);
+            close(server_fd);
+            return -1;
+        }
+    }
+
+    fclose(bmp_file);
+    close(client_fd);
+    close(server_fd);
+    return 0;
 }
 
 int main() {
-  char *ip = "127.0.0.1";
-  int port = 255666;
-  int e;
+    if (receive_bmp_over_tcp() == 0) {
+        printf("BMP file received and saved as received.bmp.\n");
+    } else {
+        printf("Failed to receive BMP file.\n");
+    }
 
-  int sockfd, new_sock;
-  struct sockaddr_in server_addr, new_addr;
-  socklen_t addr_size;
-  char buffer[SIZE];
-
-
-  sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  if (sockfd < 0) {
-    perror("[-]Error in socket");
-    exit(1);
-  }
-  printf("[+]Server socket created successfully.\n");
-
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_port = port;
-  server_addr.sin_addr.s_addr = inet_addr(ip);
-
-  e = bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
-  if (e < 0) {
-    perror("[-]Error in bind");
-    exit(1);
-  }
-  printf("[+]Binding successfull.\n");
-
-  if (listen(sockfd, 10) == 0) {
-    printf("[+]Listening....\n");
-  } else {
-    perror("[-]Error in listening");
-    exit(1);
-  }
-
-  addr_size = sizeof(new_addr);
-  new_sock = accept(sockfd, (struct sockaddr *)&new_addr, &addr_size);
-
-  printf("Reading Picture Size\n");
-  int size;
-  read(new_sock, &size, sizeof(int));
-  printf("[+]Reading file of size: %d\n", size);
-  write_file(new_sock, size);
-  printf("[+]Data written in the file successfully.\n");
-
-  return 0;
+    return 0;
 }
